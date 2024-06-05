05934a54ff18047392834136cedd97ee43bd0a07
ferdyodhiambo <oferdinanddev112@gmail.com>
2024-06-05 18:12:22 +0300

    Feature add: Implemented an alternative solution

--- piscine-go-master/elementary_programming/lv1/paramcount.go
+++ piscine-go-master/elementary_programming/lv1/paramcount.go
@@ -6,7 +6,7 @@ import (
 	"github.com/01-edu/z01"
 )
 
-func main() {
+func mains() {
 	Args := os.Args[1:]
 	count := 0
 	for i := 0; i < len(Args); i++ {
@@ -15,3 +15,32 @@ func main() {
 	z01.PrintRune(rune(count) + '0')
 	z01.PrintRune('\n')
 }
+
+// Alternative of solving the question in a way to handle large numbers
+// package main
+
+// import (
+// 	"os"
+
+// 	"github.com/01-edu/z01"
+// )
+
+// func main() {
+// 	count := 0
+// 	Args := os.Args[1:]
+// 	for i := 0; i < len(Args); i++ {
+// 		count++
+// 	}
+// 	if count <= len(Args) {
+// 		var str string
+// 		for count > 0 {
+// 			str = string('0'+count%10) + str
+// 			count /= 10
+// 		}
+// 		for _, res := range str {
+// 			z01.PrintRune(rune(res))
+// 		}
+
+// 	}
+// 	z01.PrintRune('\n')
+// }

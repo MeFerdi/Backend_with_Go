943a21b74c6fa71015df5bcd4ec7d64f14f3904a
MeFerdi <ferdinandodhis254@gmail.com>
2024-07-18 16:09:38 +0300

    Fix: Modified functions

--- elementary_programming/lv1/paramcount.go
+++ elementary_programming/lv1/paramcount.go
@@ -26,21 +26,12 @@ import (
 )
 
 func main() {
-	count := 0
 	Args := os.Args[1:]
+	count := 0
+
 	for i := 0; i < len(Args); i++ {
 		count++
 	}
-	if count <= len(Args) {
-		var str string
-		for count > 0 {
-			str = string(count%10+'0') + str
-			count /= 10
-		}
-		for _, res := range str {
-			z01.PrintRune(res)
-		}
-
-	}
+	z01.PrintRune(rune(count) + '0')
 	z01.PrintRune('\n')
 }
